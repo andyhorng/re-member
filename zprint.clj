2e3e9c9faee70364b12a6414f7f0387d2d83b2ae
Andy Hung <andy@akohub.com>
2020-07-04 10:22:42 +0000

    Add zprint

---
+++ zprint.clj
@@ -0,0 +1,14 @@
+#!/usr/bin/env bb
+
+(require '[clojure.java.io :as io])
+(require '[clojure.string :as s])
+(require '[clojure.java.shell :refer [sh]])
+
+(do (->> (io/file (or (first *command-line-args*) "."))
+         (file-seq)
+         (filter (fn [line] (or (s/ends-with? line ".clj") (s/ends-with? line ".cljs") (s/ends-with? line ".cljc"))))
+         (map (fn [f]
+                (->> (sh "zprint" :in (io/file f))
+                     :out
+                     (spit (io/file f))))))
+    (prn "done"))
